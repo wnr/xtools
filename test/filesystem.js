3a640b59a1a59b5589c309f085520b7289955956
Lucas Wiener <lucas@wiener.se>
2013-11-09 00:06:49 +0100

    filesystem can now expand recursively

--- test/filesystem.js
+++ test/filesystem.js
@@ -83,14 +83,24 @@ describe('filesystem', function() {
   });
 
   describe('expand', function() {
-    it('should list files in a directory with valid file objects', function() {
-      var path = require('path');
-      var fs = sm.require(FILE, {
+    before(function() {
+      var self = this;
+      this.path = require('path');
+      this.p = process.env.HOME + '/test/foo/';
+      this.fs = sm.require(FILE, {
         requires: {
           'fs': {
             readdirSync: function(filename) {
               if (filename === process.env.HOME + '/test/foo') {
                 return ['.git', 'LICENSE', 'node_modules', 'link'];
+              } else if (filename === self.p + '.git') {
+                return ['test', 'config', 'hooks'];
+              } else if (filename === self.p + '.git/config') {
+                return ['deepest', 'dfile'];
+              } else if (filename === self.p + '.git/hooks') {
+                return [];
+              } else if (filename === self.p + 'node_modules') {
+                return ['test'];
               }
             },
             lstatSync: function(filename) {
@@ -102,35 +112,85 @@ describe('filesystem', function() {
                 return makeStatObject(process.env.HOME + '/test/foo/', 'node_modules', 'dir');
               } else if (filename === process.env.HOME + '/test/foo/link') {
                 return makeStatObject(process.env.HOME + '/test/foo/', 'link', 'link');
+              } else if (filename === self.p + '.git/test') {
+                return makeStatObject(self.p + '.git/', 'test', 'file');
+              } else if (filename === self.p + '.git/config') {
+                return makeStatObject(self.p + '.git/', 'config', 'dir');
+              } else if (filename === self.p + '.git/config/deepest') {
+                return makeStatObject(self.p + '.git/config/', 'deepest', 'link');
+              } else if (filename === self.p + '.git/config/dfile') {
+                return makeStatObject(self.p + '.git/config/', 'dfile', 'file');
+              } else if (filename === self.p + '.git/hooks') {
+                return makeStatObject(self.p + '.git/', 'hooks', 'dir');
+              } else if (filename === self.p + 'node_modules/test') {
+                return makeStatObject(self.p + 'node_modules/', 'test', 'file');
               }
             }
           }
         }
       });
+    });
 
-      var p = process.env.HOME + '/test/foo/';
+    it('should list files in a directory with valid file objects', function() {
       var expected = [
-        makeStatObject(p, '.git', 'dir'),
-        makeStatObject(p, 'LICENSE', 'file'),
-        makeStatObject(p, 'node_modules', 'dir'),
-        makeStatObject(p, 'link', 'link')
+        makeStatObject(this.p, '.git', 'dir'),
+        makeStatObject(this.p, 'LICENSE', 'file'),
+        makeStatObject(this.p, 'node_modules', 'dir'),
+        makeStatObject(this.p, 'link', 'link')
       ];
-      var actual = fs.expand('~/noes/../test/./foo');
+      var actual = this.fs.expand('~/noes/../test/./foo', {
+        recurse: false
+      });
+      testStatObjects(actual, expected);
+    });
+
+    it('should be able to expand directories recursively', function() {
+      var expected = [
+        makeStatObject(this.p, '.git', 'dir', [
+          makeStatObject(this.p + '.git/', 'test', 'file'),
+          makeStatObject(this.p + '.git/', 'config', 'dir', [
+            makeStatObject(this.p + '.git/config/', 'deepest', 'link'),
+            makeStatObject(this.p + '.git/config/', 'dfile', 'file')
+          ]),
+          makeStatObject(this.p + '.git/', 'hooks', 'dir', [])
+        ]),
+        makeStatObject(this.p, 'LICENSE', 'file'),
+        makeStatObject(this.p, 'node_modules', 'dir', [
+          makeStatObject(this.p + 'node_modules/', 'test', 'file')
+        ]),
+        makeStatObject(this.p, 'link', 'link')
+      ];
+      var actual = this.fs.expand('~/noes/../test/./foo');
+      debugger;
       testStatObjects(actual, expected);
     });
   });
 });
 
 function testStatObjects(actuals, expected) {
+  expect(actuals).to.have.length(expected.length);
+
   for (var i = 0; i < actuals.length; i++) {
-    expect(utils.equals(actuals[i], expected[i])).to.equal(true);
     expect(actuals[i].isDirectory()).to.equal(expected[i].isDirectory());
     expect(actuals[i].isFile()).to.equal(expected[i].isFile());
     expect(actuals[i].isSymbolicLink()).to.equal(expected[i].isSymbolicLink());
+
+    //Validate the files field (if present) and then delete it.
+    if(expected[i].files) {
+      testStatObjects(actuals[i].files, expected[i].files);
+      delete actuals[i].files && delete expected[i].files;
+    }
+
+    //Since expect.eql doesnt work with functions: delete them.
+    delete actuals[i].isDirectory && delete expected[i].isDirectory;
+    delete actuals[i].isFile && delete expected[i].isFile;
+    delete actuals[i].isSymbolicLink && delete expected[i].isSymbolicLink;
+
+    expect(actuals[i]).to.eql(expected[i]);
   }
 }
 
-function makeStatObject(path, file, type) {
+function makeStatObject(path, file, type, files) {
   var base = {
     dev: 16777220,
     mode: 16877,
@@ -146,7 +206,6 @@ function makeStatObject(path, file, type) {
     mtime: 'Wed Oct 30 2013 00: 54: 18 GMT + 0100(CET)',
     ctime: 'Wed Oct 30 2013 00: 54: 18 GMT + 0100(CET)'
   };
-
   if (type === 'dir') {
     base.isDirectory = function() {
       return true;
@@ -157,6 +216,7 @@ function makeStatObject(path, file, type) {
     base.isSymbolicLink = function() {
       return false;
     };
+    base.files = files;
   } else if (type === 'link') {
     base.isDirectory = function() {
       return false;
